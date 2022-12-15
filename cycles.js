d8fd5b77d0b953085657d2c37aecdb1fbc334eaf
Olena <7890572@ukr.net>
2022-12-15 17:39:34 +0100

    Added file cycles.js

---
+++ cycles.js
@@ -0,0 +1,55 @@
+console.group('First');
+let number = 10;
+answer = '';
+while(number < 21){
+    if(number === 20){
+        answer += number;
+    } else{
+        answer += `${number}, `;
+    }
+    number++;
+}
+console.log(answer);
+console.groupEnd();
+
+console.group('Second');
+let j = 10;
+while(j <= 20){
+    console.log(`Квадрат числа ${j * j}`);
+       j++
+}
+console.groupEnd();
+
+console.group('Third');
+for (let n = 1; n < 11; n++){
+    console.log(`${n} * 7 = ${n * 7}`);
+}
+console.groupEnd();
+
+console.group ('Fourth');
+let multi = 15;
+for (let m = 15; m <= 35; m++){
+    multi = multi * m;
+}
+console.log(multi);
+console.groupEnd();
+
+console.group('Fifth');
+let mean = 0;
+for (let z = 1; z <= 500; z++){
+    mean = (mean + z) / 500;
+}
+console.log(mean);
+console.groupEnd();
+
+console.group('Sixth');
+let index = 30;
+let sum = 0;
+while (index <= 80){
+    if (!(index % 2)){
+        sum += index;
+    }
+    index++
+}
+console.log(sum);
+console.groupEnd();
